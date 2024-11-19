f1d48236d568fafc274b15e01d2f57137ca467b8
jamalgabir <jamalgamy@gmail.com>
2024-11-19 17:58:00 +0000

    Make it batter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,6 @@
-import React, { useEffect,useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    
-    const navigate = useNavigate()
-    const [login,setLogin] = useState(false);
-
-    useEffect(() => {
-        const token = localStorage.getItem('adminToken');
-
-        // If no token exists, redirect to login
-        if (!token) {
-            setLogin(false)
-        } else {
-          setLogin(true)
-        }
-    }, [navigate]);
-
-    const handleLogout = () => {
-        localStorage.removeItem('adminToken');
-        navigate('/admin/login');
-    };
-    
-    
-    
-    
     return (
         <header>
             <nav>
@@ -35,7 +10,6 @@ const Header = () => {
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/booking">Book Now</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
-                    {login&&<button className='logout-btn' onClick={handleLogout}>Logout</button>}
                 </ul>
             </nav>
         </header>
